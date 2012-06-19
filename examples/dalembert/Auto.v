1115d68fc89c0e36b9f3fa9c479d7df10bd9818a
Sylvie Boldo <sylvie.boldo@inria.fr>
2012-06-19 10:13:00 +0200

    2 hypothèses de moins pour u1

--- examples/dalembert/Auto.v
+++ examples/dalembert/Auto.v
@@ -1,5 +1,5 @@
 Require Import Reals.
-Require Import Derive RInt Locally.
+Require Import Rcomplements Derive RInt Locally Differential.
 Require Import AutoDerive.
 Require Import ssreflect.
 
@@ -68,9 +68,90 @@ End Alpha.
 
 Parameter u1 : R -> R.
 
-Hypothesis Cu1 : forall x, continuity_pt (fun u => u1 u) x.
-Hypothesis Iu1 : forall a b, ex_RInt (fun u => u1 u) a b.
 Hypothesis Du1 : forall x, ex_derive (fun u => u1 u) x.
+
+Lemma Cu1 : forall x, continuity_pt (fun u => u1 u) x.
+intros x eps Heps.
+destruct (Du1 x) as (l,Hl).
+(* *)
+case (Req_dec l 0); intros Hleq.
+apply equiv_deriv_pt_lim_0 in Hl.
+destruct (Hl (mkposreal eps Heps)) as (d,Hd).
+exists (Rmin d 1); split.
+apply Rmin_case.
+apply cond_pos.
+apply Rlt_0_1.
+simpl in Hd; unfold dist; simpl; unfold R_dist; simpl.
+intros y (_,Hy).
+replace  (u1 y - u1 x) with ((u1 y - u1 x - l * (y - x))).
+apply Rle_lt_trans with (eps * Rabs (y - x)).
+apply Hd.
+apply Rlt_le_trans with (1:=Hy).
+apply Rmin_l.
+pattern eps at 2; rewrite <- (Rmult_1_r eps).
+apply Rmult_lt_compat_l.
+apply Heps.
+apply Rlt_le_trans with (1:=Hy).
+apply Rmin_r.
+rewrite Hleq; ring.
+(* *)
+apply equiv_deriv_pt_lim_0 in Hl.
+destruct (Hl (pos_div_2 (mkposreal eps Heps))) as (d,Hd).
+exists (Rmin d (Rmin 1 ((eps/2)/Rabs l))).
+split.
+apply Rmin_case.
+apply cond_pos.
+apply Rmin_case.
+apply Rlt_0_1.
+apply Rdiv_lt_0_compat.
+apply Rdiv_lt_0_compat.
+exact Heps.
+exact Rlt_0_2.
+now apply Rabs_pos_lt.
+simpl in Hd; unfold dist; simpl; unfold R_dist; simpl.
+intros y (_,Hy).
+replace  (u1 y - u1 x) with ((u1 y - u1 x - l * (y - x))+ (l*(y-x))) by ring.
+apply Rle_lt_trans with (1:=Rabs_triang _ _).
+rewrite (double_var eps).
+apply Rplus_lt_compat.
+apply Rle_lt_trans with (eps / 2 * Rabs (y - x)).
+apply Hd.
+apply Rlt_le_trans with (1:=Hy).
+apply Rmin_l.
+pattern (eps/2) at 2; rewrite <- (Rmult_1_r (eps/2)).
+apply Rmult_lt_compat_l.
+unfold Rdiv ; apply Rmult_lt_0_compat ; 
+  [apply Heps | apply Rinv_0_lt_compat, Rlt_0_2].
+apply Rlt_le_trans with (1:=Hy).
+apply Rle_trans with (1:=Rmin_r _ _).
+apply Rmin_l.
+rewrite Rabs_mult.
+apply Rlt_le_trans with (Rabs l *  (eps / 2 / Rabs l)).
+apply Rmult_lt_compat_l.
+now apply Rabs_pos_lt.
+apply Rlt_le_trans with (1:=Hy).
+apply Rle_trans with (1:=Rmin_r _ _).
+apply Rmin_r.
+right; field.
+now apply Rabs_no_R0.
+Qed.
+
+Lemma Iu1: forall a b, ex_RInt (fun u => u1 u) a b.
+intros a b.
+case (Rle_or_lt a b); intros H.
+apply ex_RInt_correct_3.
+apply continuity_implies_RiemannInt.
+exact H.
+intros x Hx.
+apply Cu1.
+apply ex_RInt_bound.
+apply ex_RInt_correct_3.
+apply continuity_implies_RiemannInt.
+left; exact H.
+intros x Hx.
+apply Cu1.
+Qed.
+
 
 Section Beta.
 
@@ -161,12 +242,12 @@ admit.
 admit.
 unfold gamma02.
 ring_simplify.
-rewrite Rplus_opp_r Rmult_0_r Ropp_0 Rplus_0_r.
+(* rewrite Rplus_opp_r Rmult_0_r Ropp_0 Rplus_0_r.
 rewrite RInt_null Rmult_0_r Rplus_0_r.
 apply Rplus_eq_reg_l with (- f x t).
 field_simplify.
 2: exact Zc.
-rewrite Rmult_1_r.
+rewrite Rmult_1_r.*)
 Abort.
 
 End Gamma.
