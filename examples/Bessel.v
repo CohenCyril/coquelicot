121a41d4f15411bd480b7d1a5a8da8fefae93e72
Catherine LELAY <catherine.lelay@inria.fr>
2014-02-26 13:11:51 +0100

    Unicité des fonctions de Bessel

--- examples/Bessel.v
+++ examples/Bessel.v
@@ -29,16 +29,21 @@ Require Import AutoDerive.
 Definition Bessel1_seq (n k : nat) :=
   (-1)^(k)/(INR (fact (k)) * INR (fact (n + (k)))).
 
+Lemma Bessel1_seq_neq_0 (n : nat) :
+  forall k, Bessel1_seq n k <> 0.
+Proof.
+  move => k.
+  apply Rmult_integral_contrapositive_currified.
+  apply pow_nonzero, Ropp_neq_0_compat, R1_neq_R0.
+  apply Rinv_neq_0_compat, Rmult_integral_contrapositive_currified ;
+  apply INR_fact_neq_0.
+Qed.
+
 Lemma CV_Bessel1 (n : nat) :
   CV_radius (Bessel1_seq n) = p_infty.
 Proof.
   apply CV_radius_infinite_DAlembert.
-    move => k.
-    rewrite /Bessel1_seq.
-    apply Rmult_integral_contrapositive_currified.
-    apply pow_nonzero, Ropp_neq_0_compat, R1_neq_R0.
-    apply Rinv_neq_0_compat, Rmult_integral_contrapositive_currified ;
-    apply INR_fact_neq_0.
+    by apply Bessel1_seq_neq_0.
   apply is_lim_seq_ext with (fun p => / (INR (S p) * INR (S (n + p)))).
   move => p ; rewrite /Bessel1_seq -plus_n_Sm /fact -/fact !mult_INR.
   simpl ((-1)^(S p)).
@@ -102,9 +107,7 @@ Lemma is_derive_2_Bessel1 (n : nat) (x : R) :
     + (INR (n * pred n) / 4 * (x / 2) ^ pred (pred n) * PSeries (Bessel1_seq n) ((x / 2) ^ 2))).
 Proof.
   rewrite plus_INR ?mult_INR ; simpl INR.
-  apply is_derive_ext
-    with (fun x => ((x / 2) ^ S n * PSeries (PS_derive (Bessel1_seq n)) ((x / 2) ^ 2)
-    + (INR n)/2 * (x / 2) ^ pred n * PSeries (Bessel1_seq n) ((x / 2) ^ 2))).
+  eapply is_derive_ext.
   move => y ;
   by apply sym_eq, is_derive_unique, is_derive_Bessel1.
   auto_derive.
@@ -174,7 +177,7 @@ Proof.
   by apply (lt_INR 0), lt_O_Sn.
   by apply (lt_INR 0), lt_O_Sn.
 
-  apply ex_pseries_R, ex_series_Rabs, CV_disk_inside.
+  apply CV_radius_inside.
   apply Rbar_lt_le_trans with (2 := CV_radius_plus _ _).
   rewrite /Rbar_min ; case: Rbar_le_dec => _.
   by rewrite CV_radius_incr_1 ?CV_radius_derive CV_Bessel1.
@@ -252,9 +255,9 @@ Proof.
     by (simpl ; field ; exact: Hx).
   apply f_equal.
   rewrite -PSeries_incr_1 -PSeries_scal -PSeries_plus.
-Focus 2. (* ex_pseries (PS_incr_1 (Bessel1_seq (S (S n))) (S (S n))) (x / 2) *)
+Focus 2. (* ex_pseries (PS_incr_1 (Bessel1_seq (S (S n))) (S (S n))) ((x / 2) ^ 2) *)
   by apply ex_pseries_incr_1, ex_Bessel1.
-Focus 2. (* ex_pseries (PS_incr_n (Bessel1_seq n) n) (x / 2) *)
+Focus 2. (* ex_pseries (PS_incr_n (Bessel1_seq n) n) ((x / 2) ^ 2) *)
   by apply ex_Bessel1.
   apply PSeries_ext => k.
 (* egalité *)
@@ -262,11 +265,11 @@ Focus 2. (* ex_pseries (PS_incr_n (Bessel1_seq n) n) (x / 2) *)
   case: k => [ | k] ;
   rewrite ?plus_0_r -?plus_n_Sm ?plus_Sn_m
     /fact -/fact ?mult_INR ?S_INR ?plus_INR /=.
-  rewrite /plus /zero /scal /= /mult /=.
+  rewrite plus_zero_l /scal /= /mult /=.
   field.
-  rewrite -?S_INR ; split ;
+  rewrite -S_INR ; split ;
   by [apply not_0_INR, sym_not_eq, O_S | apply INR_fact_neq_0].
-  rewrite /plus /zero /scal /= /mult /=.
+  rewrite /plus /scal /= /mult /=.
   field ;
   rewrite -?plus_INR -?S_INR ; repeat split ;
   by [apply INR_fact_neq_0 | apply not_0_INR, sym_not_eq, O_S].
@@ -312,7 +315,7 @@ Qed.
 
 (** * Unicity *)
 
-Lemma Bessel1_uniqueness (a : nat -> R) (n : nat) : 
+Lemma Bessel1_uniqueness_aux_0 (a : nat -> R) (n : nat) : 
   Rbar_lt 0 (CV_radius a) ->
   (forall x : R, Rbar_lt (Rabs x) (CV_radius a) -> x^2 * Derive_n (PSeries a) 2 x + x * Derive (PSeries a) x + (x^2 - (INR n)^2) * PSeries a x = 0)
   ->
@@ -442,4 +445,211 @@ Proof.
   apply ex_pseries_scal.
   by apply Rmult_comm.
   apply CV_radius_inside ; by apply H1.
+Qed.
+
+Lemma Bessel1_uniqueness_aux_1 (a : nat -> R) (n : nat) :
+  (a 0%nat = 0 \/ n = O) ->
+  (a 1%nat = 0 \/ n = 1%nat) ->
+  (forall k, (INR (S (S k)) ^ 2 - INR n ^ 2) * a (S (S k)) + a k = 0) ->
+  (forall k : nat, (k < n)%nat -> a k = 0) 
+  /\ (forall p : nat, a (n + 2 * p + 1)%nat = 0)
+  /\ (forall p : nat, a (n + 2 * p)%nat = Bessel1_seq n p * / 2 ^ (2 * p) * INR (fact n) * a n).
+Proof.
+  intros Ha0 Ha1 Ha.
+  assert (forall k, S (S k) <> n -> a (S (S k)) = - a k / (INR (S (S k)) ^ 2 - INR n ^ 2)).
+    intros k Hk.
+    replace (a k) with (- (INR (S (S k)) ^ 2 - INR n ^ 2) * a (S (S k))).
+    field.
+    replace (INR (S (S k)) ^ 2 - INR n ^ 2)
+    with ((INR (S (S k)) - INR n) * (INR (S (S k)) + INR n))
+    by ring.
+    apply Rmult_integral_contrapositive_currified.
+    apply Rminus_eq_contra.
+    by apply not_INR.
+    rewrite -plus_INR plus_Sn_m.
+    by apply (not_INR _ O), sym_not_eq, O_S.
+    replace (a k) 
+    with ((INR (S (S k)) ^ 2 - INR n ^ 2) * a (S (S k)) + a k - (INR (S (S k)) ^ 2 - INR n ^ 2) * a (S (S k)))
+    by ring.
+    rewrite Ha ; ring.
+  assert (forall k : nat, (k < n)%nat -> a k = 0).
+    destruct n => k Hk.
+    by apply lt_n_O in Hk.
+    case: Ha0 => // Ha0.
+    destruct n.
+    destruct k => //.
+    by apply lt_S_n, lt_n_O in Hk.
+    case: Ha1 => // Ha1.
+    move: k Hk.
+    apply (Div2.ind_0_1_SS (fun k => (k < S (S n))%nat -> a k = 0)) => // k IH Hk.
+    rewrite H.
+    rewrite IH /Rdiv.
+    ring.
+    eapply lt_trans, Hk.
+    eapply lt_trans ; apply lt_n_Sn.
+    by apply NPeano.Nat.lt_neq.
+  repeat split.
+  by [].
+  elim => [ | p IH].
+  replace (n + 2 * 0 + 1)%nat with (S n) by ring.
+  destruct n => //=.
+  case: Ha1 => // Ha1.
+  case: Ha0 => // Ha0.
+  rewrite H ; try by intuition.
+  rewrite H0 /Rdiv.
+  ring.
+  by apply lt_n_Sn.
+  replace (n + 2 * S p + 1)%nat with (S (S (n + 2 * p + 1)%nat)) by ring.
+  rewrite H ; try by intuition.
+  rewrite IH /Rdiv.
+  ring.
+  elim => [ | p IH].
+  replace (n + 2 * 0)%nat with (n) by ring.
+  rewrite /Bessel1_seq /= -plus_n_O.
+  field ; by apply INR_fact_neq_0.
+  replace (n + 2 * S p)%nat with (S (S (n + 2 * p)%nat)) by ring.
+  rewrite H ; try by intuition.
+  rewrite IH /Rdiv.
+  rewrite /Bessel1_seq -plus_n_Sm.
+  rewrite !pow_sqr /fact -/fact !mult_INR !S_INR !plus_INR /=.
+  field ; rewrite -!plus_INR -!S_INR ;
+  repeat split ;
+  try (by apply INR_fact_neq_0) ;
+  try (by apply (not_INR _ 0), sym_not_eq, O_S).
+  apply pow_nonzero, Rgt_not_eq ; apply Rmult_lt_0_compat ; by apply Rlt_0_2.
+  ring_simplify (S (S (n + (p + p)))).
+  replace (INR (n + 2 * p + 2) * INR (n + 2 * p + 2) - INR n * INR n)
+    with ((INR (n + 2 * p + 2) - INR n) * (INR (n + 2 * p + 2) + INR n))
+    by ring.
+  rewrite !plus_INR /=.
+  ring_simplify (INR n + (INR p + (INR p + 0)) + 2 - INR n).
+  ring_simplify (INR n + (INR p + (INR p + 0)) + 2 + INR n).
+  rewrite -!(mult_INR 2) -plus_INR -!(plus_INR _ 2) -!plus_n_Sm plus_0_r.
+  apply Rmult_integral_contrapositive_currified ;
+  by apply (not_INR _ 0), sym_not_eq, O_S.
+Qed.
+
+Lemma Bessel1_uniqueness (a : nat -> R) (n : nat) :
+  (Rbar_lt 0 (CV_radius a)) ->
+  (forall x : R, x^2 * Derive_n (PSeries a) 2 x + x * Derive (PSeries a) x + (x^2 - (INR n)^2) * PSeries a x = 0)
+  -> {b : R | forall x, PSeries a x = b * Bessel1 n x}.
+Proof.
+  intros Hcv_a Ha.
+  assert ((a 0%nat = 0 \/ n = O) /\
+    (a 1%nat = 0 \/ n = 1%nat) /\
+    (forall k, (INR (S (S k)) ^ 2 - INR n ^ 2) * a (S (S k)) + a k = 0)).
+  by apply Bessel1_uniqueness_aux_0.
+  assert ((forall k : nat, (k < n)%nat -> a k = 0) 
+  /\ (forall p : nat, a (n + 2 * p + 1)%nat = 0)
+  /\ (forall p : nat, a (n + 2 * p)%nat = Bessel1_seq n p * / 2 ^ (2 * p) * INR (fact n) * a n)).
+  apply Bessel1_uniqueness_aux_1 ; by apply H.
+  exists (2^n * INR (fact n) * a n) => x.
+  rewrite /Bessel1 (PSeries_decr_n_aux _ n).
+  case: H0 => _ H0.
+  rewrite Rpow_mult_distr -Rinv_pow.
+  field_simplify ; rewrite ?Rdiv_1.
+  rewrite !(Rmult_assoc (x ^ n)).
+  apply Rmult_eq_compat_l.
+  rewrite PSeries_odd_even.
+  replace (PSeries (fun n0 : nat => PS_decr_n a n (2 * n0 + 1)) (x ^ 2))
+  with 0.
+  case: H0 => _ H0.
+  rewrite Rmult_0_r Rplus_0_r.
+  rewrite -PSeries_scal.
+  apply Series_ext => k.
+  rewrite /PS_decr_n /PS_scal.
+  rewrite H0.
+  rewrite -!pow_mult.
+  rewrite Rpow_mult_distr -Rinv_pow.
+  rewrite /scal /= /mult /=.
+  ring.
+  by apply Rgt_not_eq, Rlt_0_2.
+  apply sym_eq.
+  rewrite -(PSeries_const_0 (x^2)).
+  apply PSeries_ext => k.
+  rewrite /PS_decr_n.
+  replace (n + (2 * k + 1))%nat with (n + 2 * k + 1)%nat by ring.
+  by apply H0.
+  eapply ex_pseries_ext.
+  move => p ;
+  apply sym_eq.
+  apply H0.
+  eapply ex_pseries_ext.
+  intros p ; rewrite Rmult_assoc ; apply Rmult_comm.
+  apply @ex_pseries_scal.
+  by apply Rmult_comm.
+  case: (Req_dec x 0) => Hx0.
+  rewrite Hx0.
+  rewrite /= Rmult_0_l.
+  by apply @ex_pseries_0.
+  apply ex_series_Rabs.
+  apply ex_series_DAlembert with 0.
+  by apply Rlt_0_1.
+  intros p.
+  apply Rmult_integral_contrapositive_currified.
+  rewrite pow_n_pow.
+  by apply pow_nonzero, pow_nonzero.
+  apply Rmult_integral_contrapositive_currified.
+  by apply Bessel1_seq_neq_0.
+  apply Rinv_neq_0_compat.
+  apply pow_nonzero.
+  by apply Rgt_not_eq, Rlt_0_2.
+  apply is_lim_seq_ext with (fun p => x^2 / 4 * / (INR (S p) * INR (S (n + p)))).
+  intros p ; rewrite !pow_n_pow !pow_mult.
+  rewrite /Bessel1_seq -plus_n_Sm /fact -/fact !mult_INR.
+  replace (@scal R_AbsRing R_NormedModule) with Rmult by auto.
+  simpl (_^(S p)) ; rewrite -!/(pow _ 2) ; ring_simplify (2^2).
+  field_simplify (x ^ 2 * (x ^ 2) ^ p *
+   (-1 * (-1) ^ p /
+    (INR (S p) * INR (fact p) * (INR (S (n + p)) * INR (fact (n + p)))) *
+    / (4 * 4 ^ p)) /
+   ((x ^ 2) ^ p * ((-1) ^ p / (INR (fact p) * INR (fact (n + p))) * / 4 ^ p))).
+  rewrite Rabs_div.
+  rewrite Rabs_Ropp /Rdiv !Rabs_pos_eq.
+  field.
+  split ; apply (not_INR _ 0), sym_not_eq, O_S.
+  repeat apply Rmult_le_pos ; (replace 2 with (INR 2) by (simpl ; ring)) ; apply pos_INR.
+  by apply pow2_ge_0.
+  apply Rgt_not_eq ; repeat apply Rmult_lt_0_compat ;
+  (replace 2 with (INR 2) by (simpl ; ring)) ;
+  apply lt_0_INR, lt_O_Sn.
+  repeat split.
+  apply pow_nonzero, Rgt_not_eq ; repeat apply Rmult_lt_0_compat ; apply Rlt_0_2.
+  by apply INR_fact_neq_0.
+  by apply INR_fact_neq_0.
+  by apply Rgt_not_eq, lt_0_INR, lt_O_Sn.
+  by apply Rgt_not_eq, lt_0_INR, lt_O_Sn.
+  by apply pow_nonzero, Rlt_not_eq, (IZR_lt (-1) 0).
+  rewrite -pow_mult ; by apply pow_nonzero.
+  evar_last.
+  apply is_lim_seq_scal_l.
+  apply is_lim_seq_inv.
+  eapply is_lim_seq_mult.
+  apply -> is_lim_seq_incr_1.
+  by apply is_lim_seq_INR.
+  apply is_lim_seq_ext with (fun k => INR (k + S n)).
+  intros k.
+  by rewrite (Plus.plus_comm n k) plus_n_Sm.
+  apply is_lim_seq_incr_n.
+  by apply is_lim_seq_INR.
+  by [].
+  by [].
+  simpl ; apply f_equal ; ring.
+  apply ex_pseries_ext with (fun  _ => 0).
+  intros k.
+  rewrite /PS_decr_n /=.
+  replace (n + (k + (k + 0) + 1))%nat with (n + 2 * k + 1)%nat by ring.
+  by rewrite (proj1 H0).
+  eapply ex_series_ext.
+  intros k.
+  rewrite /scal /= /mult /= Rmult_0_r.
+  reflexivity.
+  exists 0 ; apply filterlim_ext with (fun _ => 0).
+  elim => /= [ | k IH].
+  by [].
+  by rewrite plus_zero_r.
+  by apply filterlim_const.
+  by apply pow_nonzero, Rgt_not_eq, Rlt_0_2.
+  by apply Rgt_not_eq, Rlt_0_2.
+  by apply H0.
 Qed.
