39bcfe61f09e2c15bf6d9098fcfde9892da0f834
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2015-03-31 15:12:26 +0200

    Prove bounded_continuity without SF_seq.

--- theories/Continuity.v
+++ theories/Continuity.v
@@ -22,7 +22,7 @@ COPYING file for more details.
 Require Import Reals.
 Require Import Ssreflect.ssreflect.
 Require Import Rcomplements Rbar Hierarchy.
-Require Import Compactness Lim_seq SF_seq.
+Require Import Compactness Lim_seq.
 
 (** This file describes defineitions and properties of continuity on
 [R] and on uniform spaces. It also contains many results about the
@@ -836,11 +836,21 @@ Proof.
   by apply f_equal, Rle_antisym.
 Qed.
 
-Lemma continue_bounded {K : AbsRing} {V : NormedModule K}
+Lemma bounded_continuity {K : AbsRing} {V : NormedModule K}
   (f : R -> V) a b :
   (forall x, a <= x <= b -> filterlim f (locally x) (locally (f x)))
   -> {M : R | forall x, a <= x <= b -> norm (f x) < M}.
 Proof.
+  destruct (Rle_dec b a) as [Hab|Hab].
+    exists (norm (f a) + 1).
+    intros x [Hax Hxb].
+    replace x with a.
+    rewrite -{1}(Rplus_0_r (norm (f a))).
+    apply Rplus_lt_compat_l, Rlt_0_1.
+    apply Rle_antisym with (1 := Hax).
+    now apply Rle_trans with b.
+  apply Rnot_le_lt in Hab.
+
   wlog: f / (forall x, filterlim f (locally x) (locally (f x))) => [ Hw Cf | Cf _ ].
     destruct (C0_extension_le f a b) as [g [Cg Hg]].
     by apply Cf.
@@ -862,14 +872,16 @@ Proof.
 
   set delta := (fun (x : R) => proj1_sig (locally_ex_dec x _ (H0 x) (H x))).
   destruct (compactness_value_1d a b delta) as [d Hd].
-  destruct (seq_step_unif_part_ex a b d) as [n Hn].
-  set l := unif_part a b n.
-  set M := (fix g l := match l with | (h :: t)%list => Rmax (norm (f h) + 2) (g t) | _ => 0 end) l.
-  exists M ; intros.
+  destruct (nfloor_ex ((b - a) / d)) as [N HN].
+    apply Rdiv_le_0_compat.
+    now apply Rlt_le, Rlt_Rminus.
+    apply cond_pos.
+  set (M := (fix g n := match n with O => 0 | S n => Rmax (norm (f (a + INR n * d)) + 3) (g n) end) (S N)).
+  exists M => x Hx.
   apply Rnot_le_lt => H2.
-  apply (Hd x H1) ; case => t.
+  apply (Hd x Hx) ; case => t.
   unfold delta.
-  case: locally_ex_dec => e He /= [Ht [Hxt Hde]].
+  case: locally_ex_dec => e /= He [Ht [Hxt Hde]].
   contradict H2 ; apply Rlt_not_le.
   move: (fun (y : R) Hy => He y (norm_compat1 _ _ _ Hy)) => {He} He.
   apply He in Hxt.
@@ -881,75 +893,62 @@ Proof.
   apply Rplus_lt_compat_r.
   by apply Hxt.
   rewrite Rplus_comm ; apply Rlt_minus_r.
-  destruct (unif_part_nat a b n t Ht).
-  - case: s => i Hi.
-    replace (norm (f t)) with ((norm (f t) - norm (f (seq.nth 0 l i))) + norm (f (seq.nth 0 l i)))%R by ring.
-    eapply Rle_lt_trans.
-    apply Rabs_triang.
-    eapply Rlt_le_trans.
-    apply Rplus_lt_compat_r.
-    rewrite -Rabs_Ropp Ropp_minus_distr'.
-    apply He.
-    rewrite -norm_opp opp_minus /norm /minus /= /abs /plus /opp /=.
-    rewrite Rabs_pos_eq.
-    eapply Rlt_trans.
-    apply Rplus_lt_compat_r.
-    by apply Hi.
-    eapply Rlt_le_trans, Hde.
-    eapply Rle_lt_trans, Hn.
-    eapply Rle_trans, nth_le_seq_step.
-    apply Rle_abs.
-    eapply lt_trans, Hi.
-    by apply lt_n_Sn.
-    rewrite -Rminus_le_0 ; apply Hi.
-    rewrite Rplus_comm.
-    apply Rle_minus_r.
-    rewrite Rplus_assoc.
-    rewrite Rabs_pos_eq.
-    2: by apply norm_ge_0.
-    rewrite -/l in Hi.
-    revert M ; elim: (l) (i) (proj2 Hi) => {l i Hi} /= [ | x0 l IH] i Hi.
-    by apply lt_n_O in Hi.
-    apply lt_S_n in Hi.
-    destruct i as [ | i] ; simpl.
-    by apply Rmax_l.
-    eapply Rle_trans, Rmax_r.
-    by apply (IH i).
-  replace (norm (f t)) with ((norm (f t) - norm (f (seq.nth 0 l n))) + norm (f (seq.nth 0 l n)))%R by ring.
-    eapply Rle_lt_trans.
-    apply Rabs_triang.
-    eapply Rlt_le_trans.
-    apply Rplus_lt_compat_r.
-    rewrite -Rabs_Ropp Ropp_minus_distr'.
-    apply He.
-    set tmp := (seq.nth 0 l n).
-    rewrite -norm_opp opp_minus /norm /= /minus /abs /plus /opp /=.
-    simpl.
-    rewrite Rabs_pos_eq.
-    eapply Rle_lt_trans.
-    apply Rplus_le_compat_r.
-    by apply a0.
-    eapply Rlt_le_trans, Hde.
-    eapply Rle_lt_trans, Hn.
-    eapply Rle_trans, nth_le_seq_step.
-    apply Rle_abs.
-    rewrite seq.size_mkseq /= ; by apply lt_n_Sn.
-    rewrite -Rminus_le_0 ; apply a0.
-    rewrite Rplus_comm.
-    apply Rle_minus_r.
-    rewrite Rplus_assoc.
-    rewrite Rabs_pos_eq.
-    2: by apply norm_ge_0.
-    assert (S n < seq.size l)%nat.
-    rewrite seq.size_mkseq ; apply lt_n_Sn.
-    revert M ; elim: (l) (n) H2 => {l n a0 Hn} /= [ | x0 l IH] n Hl.
-    by apply lt_n_O in Hl.
-    apply lt_S_n in Hl.
-    destruct n ; simpl.
-    by apply Rmax_l.
-    eapply Rle_trans, Rmax_r.
-    simpl in IH.
-    by apply IH.
+  clear x Hx Hxt.
+  destruct (nfloor_ex ((t - a) / d)) as [n Hn].
+    apply Rdiv_le_0_compat.
+    apply Rplus_le_reg_r with a.
+    now ring_simplify.
+    apply cond_pos.
+  set (y := a + INR n * d).
+  replace (norm (f t)) with (-(norm (f y) - norm (f t)) + norm (f y))%R by ring.
+  eapply Rle_lt_trans.
+
+  apply Rabs_triang.
+  eapply Rlt_trans.
+  apply Rplus_lt_compat_r.
+  rewrite Rabs_Ropp.
+  apply He.
+  change (Rabs (a + INR n * d - t) < e).
+  replace (a + INR n * d - t) with (-((t - a) / d - INR n) * d).
+  rewrite Rabs_mult (Rabs_pos_eq d).
+  2: apply Rlt_le, cond_pos.
+  apply Rlt_le_trans with (1 * d).
+  apply Rmult_lt_compat_r with (1 := cond_pos d).
+  rewrite Rabs_Ropp Rabs_pos_eq.
+  apply Rplus_lt_reg_r with (INR n).
+  now rewrite /Rminus Rplus_assoc Rplus_opp_l Rplus_0_r (Rplus_comm 1).
+  apply Rplus_le_reg_r with (INR n).
+  now ring_simplify.
+  now rewrite Rmult_1_l.
+  field.
+  apply Rgt_not_eq, cond_pos.
+
+  apply Rplus_lt_reg_l with 1.
+  ring_simplify.
+  rewrite -> Rabs_pos_eq by apply norm_ge_0.
+  assert (n < S N)%nat.
+  apply INR_lt.
+  apply Rle_lt_trans with (1 := proj1 Hn).
+  rewrite S_INR.
+  apply Rle_lt_trans with (2 := proj2 HN).
+  apply Rmult_le_compat_r.
+  apply Rlt_le, Rinv_0_lt_compat, cond_pos.
+  now apply Rplus_le_compat_r.
+  unfold M, y.
+  clear -H1.
+  induction N.
+  apply Rlt_le_trans with (2 := Rmax_l _ _).
+  destruct n.
+  apply Rplus_lt_compat_l, Rplus_lt_compat_l.
+  now apply (IZR_lt 1 2).
+  now apply lt_S_n in H1.
+  destruct (le_lt_eq_dec _ _ (lt_n_Sm_le _ _ H1)) as [H2|H2].
+  apply Rlt_le_trans with (2 := Rmax_r _ _).
+  now apply IHN.
+  apply Rlt_le_trans with (2 := Rmax_l _ _).
+  rewrite H2.
+  apply Rplus_lt_compat_l, Rplus_lt_compat_l.
+  now apply (IZR_lt 1 2).
 Qed.
 
 (** *** Order *)
