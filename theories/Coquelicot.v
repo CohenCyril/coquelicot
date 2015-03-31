60faccdc961d347d3dab4a2e5ad04f8ea6a21de0
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2015-03-31 15:13:04 +0200

    Add RInt_gen and RInt_analysis to Coquelicot.

--- theories/Coquelicot.v
+++ theories/Coquelicot.v
@@ -270,7 +270,7 @@ library is provided by lemmas [is_series_Reals] and [is_pseries_Reals].
 Require Export AutoDerive Compactness Complex Continuity Derive.
 Require Export Derive_2d Equiv ElemFct Hierarchy Lim_seq.
 Require Export Lub Markov PSeries Rbar Rcomplements.
-Require Export RInt Seq_fct Series SF_seq.
+Require Export RInt RInt_gen RInt_analysis Seq_fct Series SF_seq.
 
 (** #<img src="deps.png" usemap="##coquelicot_deps" /># *)
 
